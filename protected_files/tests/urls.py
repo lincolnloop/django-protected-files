2157fc6564cf87537a0d0aa1ddabf1f0ab14f091
Peter Baumgartner <pete@lincolnloop.com>
2009-05-15 09:13:20 -0600

    minor style tweak

--- protected_files/tests/urls.py
+++ protected_files/tests/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^perms/(?P<file_path>.*)', 'protected_files.views.protected_file', {'perm':'auth.add_user'}),
-    url(r'^alt-root/(?P<file_path>.*)', 'protected_files.views.protected_file', {'perm':'auth.add_user', 'redirect_root': '/alt/root/path'}),
+    url(r'^alt-root/(?P<file_path>.*)', 'protected_files.views.protected_file', {'perm':'auth.add_user', 'redirect_root':'/alt/root/path'}),
     url(r'^no-perms/(?P<file_path>.*)', 'protected_files.views.protected_file'),
     
 )
